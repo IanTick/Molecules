d7dd955defe57836176829b7663630e51f7f6301
IanTick <IanTick@protonmail.com>
2022-05-24 11:09:50 +0200

    Building fup collection

--- src/collections/MlcVec.rs
+++ src/collections/MlcVec.rs
@@ -3,7 +3,7 @@ use crate::primitives::AtomicCell::*;
 use std::sync::Arc;
 
 pub struct MlcVec<T> {
-    pub(crate) beam: AtomicCell<Vec<Arc<AtomicCell<T>>>>,
+    pub(crate) beam: AtomicCell<Vec<Arc<T>>>,
 }
 
 impl<T> MlcVec<T> {
@@ -13,21 +13,27 @@ impl<T> MlcVec<T> {
         }
     }
 
-    pub fn from<O>(mut iterable: O) -> Self
-    where
-        O: Iterator<Item = T>,
-    {
-        let mut vector = Vec::new();
-        while let Some(element) = iterable.next() {
-            vector.push(Arc::new(AtomicCell::new(element)));
-        }
-        Self {
-            beam: AtomicCell::new(vector),
+    pub fn get(&self, idx: usize) -> Option<Arc<T>> {
+        match self.beam.load().get(idx) {
+            Some(z) => Some(z.clone()),
+            None => None,
         }
     }
 
-    pub fn get(&self, idx: usize) -> Option<Arc<AtomicCell<T>>> {
-        Some((*self.beam.load()).get(idx)?.clone())
+    pub fn push(&self, new: Arc<T>) {
+        self.beam.fetch_update::<T, _>(|vec| {
+            let mut next_vec = (*vec).clone();
+            next_vec.push(new.clone());
+            (next_vec, None)
+        });
+    }
+
+    pub fn pop(&self) -> Option<Arc<T>> {
+        self.beam.fetch_update(|vec| {
+            let mut next_vec = (*vec).clone();
+            let output = next_vec.pop();
+            (next_vec, Some(output))
+        }).unwrap()
     }
 
     /*pub fn push(&self, value: T) {
@@ -60,5 +66,4 @@ impl<T> MlcVec<T> {
             owned_vec.remove(idx);
         }
     }*/
-    pub fn pop() {}
 }
